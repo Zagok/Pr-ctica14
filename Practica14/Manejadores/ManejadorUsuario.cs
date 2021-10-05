bae168bd0e2c8fc9fd385f83f43b0df842d1c963
Zagok <gomezcarlos1524@outlook.com>
2021-10-05 09:21:49 -0500

    COMMIT ANTES DE LOS PERFILES

--- Practica14/Manejadores/ManejadorUsuario.cs
+++ Practica14/Manejadores/ManejadorUsuario.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AccesoDatos;
+﻿using AccesoDatos;
 using System.Windows.Forms;
 using Entidades;
 
